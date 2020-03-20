c4713215d5cfed33e0ed94245a7a5928306347d4
websitehome <61357047+websitehome@users.noreply.github.com>
2020-03-20 14:02:22 +0200

    First Commit

---
+++ submit.php
@@ -0,0 +1,20 @@
+<?php
+if (!empty ($_POST['email']) AND !empty($_POST['name']) AND !empty($_POST['message'])){
+
+  $theme = 'Новое сообщение';
+
+  $letter = 'Данные Сообщения:r\n';
+  $letter .='Email'.$_POST['email']'r\n';
+  $letter .='Name'.$_POST['name']'r\n';
+  $letter .='message'.$_POST['message']'r\n';
+  if(mail('dimf29@gmail.com',$theme,$letter)){
+    header('Location:/thankyou.html');
+  }else{
+    header('Location:/');
+  }
+}
+else{
+    header('Location:/');
+}
+
+ ?>
