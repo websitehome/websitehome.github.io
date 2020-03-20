9bd6c4b0566451748519cc4a19ec1cf98454ab5a
websitehome <61357047+websitehome@users.noreply.github.com>
2020-03-20 21:50:08 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,16 +1,14 @@
 <?php
 if (!empty ($_POST['email']) AND !empty($_POST['name']) AND !empty($_POST['message'])){
-
+   
   $theme = 'Новое сообщение';
 
-  $letter = 'Данные Сообщения:r\n';
+  $letter = 'Данные Сообщения:'r\n';
   $letter .='Email'.$_POST['email']'r\n';
   $letter .='Name'.$_POST['name']'r\n';
   $letter .='message'.$_POST['message']'r\n';
   if(mail('dimf29@gmail.com',$theme,$letter)){
-    header('Location:thankyou.html');
-  }else{
-    header('Location:/');
+    header('Location:/thankyou.html');
   }
 }
 else{
